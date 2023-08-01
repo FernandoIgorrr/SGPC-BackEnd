74ee40b5407d57386dd45b7fd4b60d2855b7bc7c
Fernando Igor <fernando_id.7@hotmail.com>
2023-08-01 19:15:46 -0300

    commit inicial (criação do projeto com o spring initializr)

---
+++ src/main/java/com/api/sgpcbackend/SgpcBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.api.sgpcbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgpcBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SgpcBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/api/sgpcbackend/SgpcBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.sgpcbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgpcBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
